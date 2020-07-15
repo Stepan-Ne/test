0f015b01fdf70e5dd4704a05677070778b2f5d33
Stepan-Ne <nemail.necom@gmail.com>
2020-07-15 22:56:23 +0300

    show random person

--- Review/app.js
+++ Review/app.js
@@ -1,77 +1,82 @@
 // local reviews data
 const reviews = [
-    {
-      id: 1,
-      name: "susan smith",
-      job: "web developer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    },
-    {
-      id: 2,
-      name: "anna johnson",
-      job: "web designer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-     },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      }
-  ];
-  //select items
-  const img = document.getElementById('person-img')
-  const author = document.getElementById('author')
-  const job = document.getElementById('job')
-  
-
-  const prevBtn = document.querySelector('.prev-btn')
-  const nextBtn = document.querySelector('.next-btn')
-  const randomBtn = document.querySelector('.surprice')
-
-  //set starting item
-  let currentValue = 0
-
-  //load initial item 
-  window.addEventListener('DOMContentLoaded', function() {
-    showPerson(currentValue)
-  })
-
-  //show person based on item
-  function showPerson(person) {
-    const item = reviews[person]
-    img.src = item.img
-    author.textContent = item.name
-    job.textContent = item.job 
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   }
+];
+//select items
+const img = document.getElementById('person-img')
+const author = document.getElementById('author')
+const job = document.getElementById('job')
 
-  //show next person
-  nextBtn.addEventListener('click', function() {
-    currentValue++
-    if (currentValue > reviews.length - 1) {
-        currentValue = 0
-        showPerson(currentValue)
-    } else {
-        showPerson(currentValue)
-    }
-  })
-  //show prev person
-  prevBtn.addEventListener('click', function() {
-    currentValue--
-    if (currentValue < 0) {
-        currentValue = reviews.length - 1
-        showPerson(currentValue)
-    } else {
-        showPerson(currentValue)
-    }
-  })
+
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const randomBtn = document.querySelector('.surprice')
+
+//set starting item
+let currentValue = 0
+
+//load initial item 
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currentValue)
+})
+
+//show person based on item
+function showPerson(person) {
+  const item = reviews[person]
+  img.src = item.img
+  author.textContent = item.name
+  job.textContent = item.job
+}
+
+//show next person
+nextBtn.addEventListener('click', function () {
+  currentValue++
+  if (currentValue > reviews.length - 1) {
+    currentValue = 0
+    showPerson(currentValue)
+  } else {
+    showPerson(currentValue)
+  }
+})
+//show prev person
+prevBtn.addEventListener('click', function () {
+  currentValue--
+  if (currentValue < 0) {
+    currentValue = reviews.length - 1
+    showPerson(currentValue)
+  } else {
+    showPerson(currentValue)
+  }
+})
+//show random person
+randomBtn.addEventListener('click', function () {
+  currentValue = Math.floor(Math.random() * reviews.length)
+  showPerson(currentValue)
+})
